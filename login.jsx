e6c50a0edb3fc0ce9431c5abf30dd6308e33f9d4
Nandana B Pillai <90125324+Nandanabpillai@users.noreply.github.com>
2023-08-28 12:26:06 +0530

    Add files via upload

--- login.jsx
+++ login.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import './style.css'; // Make sure to import your stylesheet(s)
-import './login.css'
 import { useState, useEffect } from "react"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "./firebase"
 import { Link, useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useUserAuth } from "./UserAuthContext";
+import './login.css'
 
-export default function LoginPage() {
-  const [email, setEmail] = useState('')
+export default function Login(props) {
+    const [email, setEmail] = useState('')
     const [pass, setPass] = useState('')
+    const auth = getAuth()
     const [err, setErr] = useState("")
     const navigate = useNavigate()
     const { logIn } = useUserAuth
-      const auth = getAuth()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // setErr("")
+        // try {
+        //     await logIn(email, pass)
+        //     console.log(email)
+        //     navigate("/home")
+        // } catch(err) {
+        //     setErr(err.message)
+        // }
         signInWithEmailAndPassword(auth, email, pass)
         .then((userCredential) => {
             // Signed in
@@ -31,76 +37,65 @@ export default function LoginPage() {
             console.log(errorCode, errorMessage)
         })
     };
-  return (
-    <div>
 
-      <div className="login-container">
-        <div className="login-login">
-          <span className="login-text"><span>Welcome Back .!</span></span>
-          <img
-            src="/external/ellipse1166-wiqo-400h.png"
-            alt="Ellipse1166"
-            className="login-ellipse1"
-          />
-          <img
-            src="/external/ellipse2167-fv9m-300h.png"
-            alt="Ellipse2167"
-            className="login-ellipse2"
-          />
-          <form className = 'login-form' onSubmit={handleSubmit}>
-          <div className="login-frame2">
-            <div className="login-frame5">
-              <div className="login-frame4">
-                <div className="login-upper-section">
-                  <div className="login-logintext">
-                    <span className="login-text02"><span>Login</span></span>
-                    <span className="login-text04">
-                      <span>Glad you’re back.!</span>
-                    </span>
-                  </div>
-                  <div className="login-credentials">
-                  {/*<label htmlFor = 'email'>email</label>*/}
-                    <div className="login-username">
-                      <span className="login-text06">
-                        <input value = {email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="Email" placeholder="email"></input>
-                      </span>
+    return (
+        <div>
+            <div className="login-container">
+                <div className="login-login">
+                    <span className="login-text"><span>Welcome Back .!</span></span>
+                    <img
+                        src="public/external/ellipse1166-wiqo-400h.png"
+                        alt="Ellipse1166"
+                        className="login-ellipse1"
+                    />
+                    <img
+                        src="public/external/ellipse2167-fv9m-300h.png"
+                        alt="Ellipse2167"
+                        className="login-ellipse2"
+                    />
+                    <div className="login-frame2">
+                        <div className="login-frame5">
+                            <div className="login-frame4">
+                                <div className="login-upper-section">
+                                    <div className="login-logintext">
+                                        <span className="login-text02"><span>Login</span></span>
+                                        <span className="login-text04">
+                                            <span>Glad you’re back.!</span>
+                                        </span>
+                                    </div>
+                                    <form onSubmit={handleSubmit} className="login-credentials">
+                                        <div className="login-password-rem">
+                                            <label className="login-text06" htmlFor = 'email'>Email</label>
+                                            <input className="login-username" value = {email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email"></input>
+                                        </div>
+                                        <div className="login-password-rem">
+                                                <label className="login-text08" htmlFor = 'password'>Password</label>
+                                                <input className="login-password" value = {pass} onChange={(e) => setPass(e.target.value)}type="password" id="password" name="password"></input>
+                                        </div>
+                                        
+                                                <button className="login-login1" type="submit">Login</button>
+                                       
+                                            {/* <span className="login-text14">
+                                                <span>Forgot password ?</span>
+                                            </span> */}
+                                        
+                                    </form>
+                                </div>
+                            </div>
+                            <div className="login-frame9">
+                                <span className="login-text16">
+                                    <button className = "link-btn"> Don't have an account? <Link to = "/signup">Register here</Link></button>
+                                </span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="login-password-rem">
-                      <div className="login-password">
-                        <span className="login-text08">
-                        <input value = {pass} onChange={(e) => setPass(e.target.value)}type="password" id="password" name="Password'></input></span>
-                        <img
-                          src="/external/closedeye181-apii.svg"
-                          alt="closedeye181"
-                          className="login-closedeye"
-                        />
-                      </div>
-                    </div>
-                    <div className="login-loginbt-fp">
-                      <div className="login-login1">
-                        <span className="login-text12"><button className = 'l' type = 'submit'>Login</button></span>
-                      </div>
-                  </div>
+                    <img
+                        src="public/external/line31031-fuot7.svg"
+                        alt="Line31031"
+                        className="login-line3"
+                    />
                 </div>
-              </div>
-              
-              <div className="login-frame9">
-                <span className="login-text16">
-                  <span>Don’t have an account ? <Link to = "/signup">Register here</Link></span>
-                </span>
-              </div>
             </div>
-          </div>
-          </div>
-          </form>
-          <img
-            src="/external/line31031-fuot7.svg"
-            alt="Line31031"
-            className="login-line3"
-          />
-        </div>
-      </div>
-    </div>
-  );
+        </div>          
+    )
 }
-
--- register.jsx
+++ register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { Link, useNavigate } from 'react-router-dom'
 import { useUserAuth } from "./UserAuthContext";
+import './index.css';
 
 export default function Register(props) {
     const [email, setEmail] = useState('')
@@ -35,18 +36,81 @@ export default function Register(props) {
     }
 
     return (
-        <div className="form-container">
-            <h2> Register </h2>
-            <form className = 'reg-form' onSubmit={handleSubmit}>
-                <label htmlFor = 'name'>user name</label>
-                <input value = {name} onChange={(e) => setName(e.target.value)} id="name" name="name"></input>
-                <label htmlFor = 'email'>email</label>
-                <input value = {email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email"></input>
-                <label htmlFor = 'password'>password</label>
-                <input value = {pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password"></input>
-                <button type="submit"> Register </button>
-            </form>
-            <button className = "link-btn"> Already have an account? <Link to = "/">Login here</Link></button>
+        <div>
+
+            <div className="signup-container">
+                <div className="signup-signup">
+                    <span className="signup-text"><span>Roll the Carpet.!</span></span>
+                    <img
+                        src="/external/ellipse118-5gv-400h.png"
+                        alt="Ellipse118"
+                        className="signup-ellipse1"
+                    />
+                    <img
+                        src="/external/ellipse219-evi-300h.png"
+                        alt="Ellipse219"
+                        className="signup-ellipse2"
+                    />
+                <div className="signup-frame2">
+                    <div className="signup-frame5">
+                        <div className="signup-frame4">
+                            <div className="signup-upper-section">
+                                <div className="signup-logintext">
+                                    <span className="signup-text02"><span>Signup</span></span>
+                                    <span className="signup-text04">
+                                        <span>Just some details to get you in.!</span>
+                                    </span>
+                                </div>
+                                <form className="signup-credentials" onSubmit={handleSubmit}>
+                                    <div className="signup-password-rem">
+                                        <label className="signup-text10" htmlFor = 'name'>User Name</label>
+                                        <input className="signup-username" value = {name} onChange={(e) => setName(e.target.value)} id="name" name="name"></input>
+                                   </div>
+                                   <div className="signup-password-rem">
+                                        <label className="signup-text08" htmlFor = 'email'>Email</label>
+                                        <input className="signup-username1" value = {email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email"></input>
+                                    </div>
+                                    <div className="signup-password-rem">
+                                        <label className="signup-text10" htmlFor = 'password'>Password</label>
+                                        <input className="signup-password" value = {pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password"></input> 
+                                    </div>
+                                   
+                                        <button className="signup-login" type="submit"> Register </button>
+                                </form>
+                            </div>
+                            <div className="signup-otherlogins">
+                                <div className="signup-or">
+                                    <img
+                                    src="/external/line1137-s20k.svg"
+                                    alt="Line1137"
+                                    className="signup-line1"
+                                    />
+                                    <span className="signup-text16"><span>Or</span></span>
+                                    <img
+                                    src="/external/line2139-hof9.svg"
+                                    alt="Line2139"
+                                    className="signup-line2"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="signup-frame9">
+                            <span className="signup-text18">
+                                <button className = "link-btn"> Already have an account? <Link to = "/">Login here</Link></button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <img
+                    src="/external/line3161-tf3h.svg"
+                    alt="Line3161"
+                    className="signup-line3"
+                />
+            </div>
         </div>
+    </div>
+                
+                
+            // <button className = "link-btn"> Already have an account? <Link to = "/">Login here</Link></button>
     )
 }
